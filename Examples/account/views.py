f59ce10cce14efad6c2414eceba9de1cbb919fb7
Mactto <shc9928@gmail.com>
2021-01-13 16:36:39 +0900

    updates

--- Examples/account/views.py
+++ Examples/account/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+
+def home(request):
+    return render(request, 'home.html')
 
 # Create your views here.
 def signup(request):
@@ -28,5 +31,5 @@ def login(request):
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
-        redirect('home')
+        return redirect('home')
     return render(request, 'login.html')
